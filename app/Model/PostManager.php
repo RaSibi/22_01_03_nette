1220f973bd764ce08e402553710acdde3f231d00
RaSibi <rosigma@seznam.cz>
2022-01-09 14:15:07 +0100

    Paginator added

--- app/Model/PostManager.php
+++ app/Model/PostManager.php
@@ -2,11 +2,13 @@
 
 namespace App\Model;
 
+use Nette;
 use Nette\Database\Explorer;
 use \Tracy\Debugger;
 
 class PostManager
 {
+    use Nette\SmartObject;
     protected $database;
 
     public function __construct(Explorer $database)
@@ -14,21 +16,37 @@ class PostManager
         $this->database = $database;
     }
 
-    public function getAllItems()
+    //STRANKOVANI PAGINATOR
+    public function getAllItems(int $limit, int $offset): Nette\Database\ResultSet
     {
-        $database = $this->database;
-        $database->beginTransaction();
-        $queryUvod = 'SELECT h.nickname, h.email, j.popis AS id_jezero, r.popis AS id_reka, h.klidna_voda, h.tekouci_voda, h.sprcha, h.sauna, text
+        return $this->database->query('SELECT h.nickname, h.email, j.popis AS id_jezero, r.popis AS id_reka, h.klidna_voda, h.tekouci_voda, h.sprcha, h.sauna, text
         FROM hlavni h
         LEFT JOIN jezera j ON h.id_jezero = j.id
-        LEFT JOIN reky r ON h.id_reka = r.id';
-        $rows = $database->query($queryUvod)->fetchAll();
-        $database->commit();
-        Debugger::barDump($rows);
-        return $rows; //Obvykle se vrací DTO - Data Transfer Object(y)
+        LEFT JOIN reky r ON h.id_reka = r.id
+        LIMIT ?
+        OFFSET ?', $limit, $offset);
+    }
+    public function getPublishedArticlesCount(): int
+    {
+        return $this->database->fetchField('SELECT COUNT(*) FROM hlavni');
     }
 
-    public function savePostItems($nick, $email, $klidna, $tekouci, $sprcha, $sauna, $jezero, $reka, $text)
+    // public function getAllItems()
+    // {
+    //     $database = $this->database;
+    //     $database->beginTransaction();
+    //     $queryUvod = 'SELECT h.nickname, h.email, j.popis AS id_jezero, r.popis AS id_reka, 
+    //     h.klidna_voda, h.tekouci_voda, h.sprcha, h.sauna, text
+    //     FROM hlavni h
+    //     LEFT JOIN jezera j ON h.id_jezero = j.id
+    //     LEFT JOIN reky r ON h.id_reka = r.id';
+    //     $rows = $database->query($queryUvod)->fetchAll();
+    //     $database->commit();
+    //     Debugger::barDump($rows);
+    //     return $rows; //Obvykle se vrací DTO - Data Transfer Object(y)
+    // }
+
+    public function savePostItems($nick, $email, $klidna, $tekouci, $sprcha, $sauna, $jezero, $reka, $text): void
     {
         $database = $this->database;
         $database->beginTransaction();
--- app/Presenters/GetPresenter.php
+++ app/Presenters/GetPresenter.php
@@ -6,6 +6,7 @@ namespace App\Presenters;
 use \Nette\Application\UI\Presenter;
 use App\Model\PostManager;
 use \Nette\Application\UI\Form;
+use Nette;
 
 class GetPresenter extends Presenter
 {
@@ -18,11 +19,27 @@ class GetPresenter extends Presenter
         parent::__construct();
     }
 
-    public function renderDefault()
+    public function renderDefault(int $page = 1) : void
     {
-        $this->template->postItems = $this->postManager->getAllItems();
+        $articlesCount = $this->postManager->getPublishedArticlesCount();
+        $paginator = new Nette\Utils\Paginator;
+        $paginator->setItemCount($articlesCount); // celkový počet článků
+		$paginator->setItemsPerPage(5); // počet položek na stránce
+		$paginator->setPage($page);
+        $articles = $this->postManager->getAllItems($paginator->getLength(), $paginator->getOffset());
+        $this->template->postItems = $articles;
+		// a také samotný Paginator pro zobrazení možností stránkování
+		$this->template->paginator = $paginator;
         $this->template->title = 'Přehled příspěvků ostravských otužilců';
+        
     }
+    // public function renderDefault() : void
+    // {
+    //     $this->template->postItems = $this->postManager->getAllItems();
+    //     $this->template->title = 'Přehled příspěvků ostravských otužilců';
+    // }
+
+    
 
     public function renderFiltrJmeno($jmeno)
     {
@@ -37,7 +54,7 @@ class GetPresenter extends Presenter
         $razeni = ['asc' => 'vzestupně', 'des' => 'sestupně'];
         $form->addRadioList('radit', 'Řazení', $razeni);
         $form->addSubmit('insert', 'Odeslat');
-        $form->onSuccess[] = [$this, 'getItemFilter'];
+        $form->onSuccess[] = [$this, 'getFilteredItem'];
         return $form;
     }
 }
